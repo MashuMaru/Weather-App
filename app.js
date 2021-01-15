3ddd2412e522ef1bbda3ffc5a9b9482177805e7c
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-15 07:46:14 +0000

    added bodyParser + https require

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const https = require('https');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', function(req, res) {
-    res.send('does this work?');
+    const location = req.body.cityName;
+    console.log(req.body.cityName);
+
+    res.send();
 })
 
 app.listen(port, function() {
