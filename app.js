058895157a2c4c38c758628c15eca3ecfde617a2
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-15 07:03:24 +0000

    testing Git version control

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+    res.send('does this work?');
+})
+
+app.listen(port, function() {
+    console.log('Serving on port ' + port + '.');
+    console.log('Does this work, I wonder...');
+})
+
+
