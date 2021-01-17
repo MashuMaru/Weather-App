2a1d3eccb600597579f98dd064aacd7e7fccaea4
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-17 20:53:41 +0000

    added time requested JS.

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.post('/', function(req, res) {
 
 
         response.on('data', function(data) {
-            // CURRENT TIME BELOW
+
             var date = new Date();
             var hour = date.getHours();
             var minute = date.getMinutes();
             res.write("<p>Request at time: "+hour+": "+minute+"</p>");
-            // CURRENT TIME ABOVE
+            
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const focusTemp = Math.ceil(temp);
