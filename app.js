6f9751d37f79a47caf4ac9e8f716ee23d9e6f6d4
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-17 17:18:30 +0000

    amendment to the weather API. Added time/date API.

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const https = require('https');
-const bodyParser = require("body-parser");
-const { query } = require("express");
+const https = require('https');const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -13,9 +11,12 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
     const location = req.body.cityName;
+    const continent = req.body.continentName; 
     const unit = 'metric';
     const apiKey = 'cf076bdf72cb2750703c8f245f6f0090';
     const url = 'https://api.openweathermap.org/data/2.5/weather?q='+location+'&appid='+ apiKey +'&units=' + unit + '';
+    const urlTime = 'https://worldtimeapi.org/api/timezone/'+continent+'/'+location+'';
+
     https.get(url, function(response) {
         console.log(response.statusCode);
 
@@ -26,14 +27,35 @@ app.post('/', function(req, res) {
             const description = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            console.log(location);
-            console.log(focusTemp + '°C');
-            res.write("<p>The weather is " + description + ".</p>");
+            res.write("<h1>The weather is " + description + ".</h1>");
             res.write("<h1>The temperature in " + location + " is " + focusTemp + " degress Celcius.</h1>");
             res.write("<img src=" +imgURL+">");
-            res.send();
+
+            https.get(urlTime, function(response) {
+                console.log(response.statusCode);
+                response.on('data', function(data) {
+                    const timeData = JSON.parse(data);
+                    const time = timeData.timezone;
+                    const date = timeData.datetime;
+                    res.write("<p>The time zone is "+time+".</p>");
+                    res.write("<p>The date and time is "+date+".</p>");
+                    res.send();
+                })
+            })
         })
     })
+
+    // https.get(urlTime, function(response) {
+
+    //     response.on('data', function(data) {
+    //         const timeData = JSON.parse(data);
+    //         const time = timeData.timezone;
+    //         const date = timeData.datetime;
+    //         res.write("<p>The time zone is "+time+".</p>");
+    //         res.write("<p>The date and time is "+date+".</p>");
+    //         res.send();
+    //     })
+    // })
 })
 
 app.listen(port, function() {
